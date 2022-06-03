2a51a949f99f2738ad15c7a91990fbb6b64c5b69
piatkifilip <106532560+piatkifilip@users.noreply.github.com>
2022-06-03 16:34:12 +0100

    Update main.py
    Used Qt Designer from pyqt5-tools to create thie GUI, converted the .ui file into code by using pyuic5 command

--- WEEK1/main.py
+++ WEEK1/main.py
@@ -1,1 +1,42 @@
-import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button1 = QtWidgets.QPushButton(self.centralwidget)
+        self.button1.setGeometry(QtCore.QRect(320, 400, 171, 61))
+        self.button1.setObjectName("button1")
+        self.checkBox1 = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox1.setGeometry(QtCore.QRect(130, 420, 141, 16))
+        self.checkBox1.setObjectName("checkBox1")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button1.setText(_translate("MainWindow", "Button1"))
+        self.checkBox1.setText(_translate("MainWindow", "Parameter"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
